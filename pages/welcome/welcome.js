585f1decc885d6cca0453fe25d369bf2cf4f9118
Bar RenewK Developer <dev@bar-renewk.com>
2026-05-18 02:47:48 +0000

    修复连续点击进入调酒师后台功能

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,12 +1,13 @@
 const barData = require('../../utils/data.js');
 
+let clickCount = 0;
+let clickTimer = null;
+
 Page({
   data: {
     venueName: '',
     showNewCustomerOffer: true,
     offerOptions: [],
-    clickCount: 0,
-    lastClickTime: 0,
     showHint: false
   },
 
@@ -15,43 +16,86 @@ Page({
       venueName: barData.venue.name,
       offerOptions: barData.venue.new_customer_offer.options
     });
+    clickCount = 0;
   },
 
   onShow() {
   },
 
-  handleStart() {
+  handleStart(e) {
     const now = Date.now();
-    const { lastClickTime, clickCount } = this.data;
-
-    if (now - lastClickTime < 500) {
-      const newCount = clickCount + 1;
-      this.setData({
-        clickCount: newCount,
-        lastClickTime: now
-      });
-
-      if (newCount >= 5) {
-        this.setData({ clickCount: 0, showHint: false });
-        wx.navigateTo({
-          url: '/pages/bartender/bartender'
-        });
-        return;
+    const targetText = e.currentTarget.dataset.text || '';
+    
+    if (targetText !== 'Bar Kenny') {
+      clickCount = 0;
+      if (clickTimer) {
+        clearTimeout(clickTimer);
+        clickTimer = null;
       }
-
-      if (newCount === 3) {
-        this.setData({ showHint: true });
-      }
-    } else {
-      this.setData({
-        clickCount: 1,
-        lastClickTime: now,
-        showHint: false
+      
+      wx.navigateTo({
+        url: '/pages/steps/steps'
       });
+      return;
     }
+
+    clickCount++;
+
+    if (clickTimer) {
+      clearTimeout(clickTimer);
+    }
+
+    if (clickCount >= 5) {
+      clickCount = 0;
+      wx.navigateTo({
+        url: '/pages/bartender/bartender'
+      });
+      return;
+    }
+
+    if (clickCount >= 3) {
+      this.setData({ showHint: true });
+    }
+
+    clickTimer = setTimeout(() => {
+      clickCount = 0;
+      this.setData({ showHint: false });
+    }, 2000);
 
     wx.navigateTo({
       url: '/pages/steps/steps'
     });
+  },
+
+  onTitleTap() {
+    clickCount++;
+
+    if (clickTimer) {
+      clearTimeout(clickTimer);
+    }
+
+    if (clickCount >= 5) {
+      clickCount = 0;
+      if (clickTimer) {
+        clearTimeout(clickTimer);
+        clickTimer = null;
+      }
+      wx.navigateTo({
+        url: '/pages/bartender/bartender'
+      });
+      return;
+    }
+
+    if (clickCount >= 3) {
+      wx.showToast({
+        title: `还需点击 ${5 - clickCount} 次进入后台`,
+        icon: 'none',
+        duration: 1000
+      });
+    }
+
+    clickTimer = setTimeout(() => {
+      clickCount = 0;
+    }, 2000);
   }
 });
