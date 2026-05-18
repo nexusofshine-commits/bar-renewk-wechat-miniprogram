e650b108112fd1bc43af0eebd6cc00002fa4b7c5
Bar RenewK Developer <dev@bar-renewk.com>
2026-05-18 02:54:23 +0000

    修改入口为点击酒杯icon进入调酒师后台

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -7,8 +7,7 @@ Page({
   data: {
     venueName: '',
     showNewCustomerOffer: true,
-    offerOptions: [],
-    showHint: false
+    offerOptions: []
   },
 
   onLoad() {
@@ -16,58 +15,18 @@ Page({
       venueName: barData.venue.name,
       offerOptions: barData.venue.new_customer_offer.options
     });
-    clickCount = 0;
   },
 
   onShow() {
   },
 
-  handleStart(e) {
-    const now = Date.now();
-    const targetText = e.currentTarget.dataset.text || '';
-    
-    if (targetText !== 'Bar Kenny') {
-      clickCount = 0;
-      if (clickTimer) {
-        clearTimeout(clickTimer);
-        clickTimer = null;
-      }
-      
-      wx.navigateTo({
-        url: '/pages/steps/steps'
-      });
-      return;
-    }
-
-    clickCount++;
-
-    if (clickTimer) {
-      clearTimeout(clickTimer);
-    }
-
-    if (clickCount >= 5) {
-      clickCount = 0;
-      wx.navigateTo({
-        url: '/pages/bartender/bartender'
-      });
-      return;
-    }
-
-    if (clickCount >= 3) {
-      this.setData({ showHint: true });
-    }
-
-    clickTimer = setTimeout(() => {
-      clickCount = 0;
-      this.setData({ showHint: false });
-    }, 2000);
-
+  handleStart() {
     wx.navigateTo({
       url: '/pages/steps/steps'
     });
   },
 
-  onTitleTap() {
+  onIconTap() {
     clickCount++;
 
     if (clickTimer) {
@@ -86,13 +45,11 @@ Page({
       return;
     }
 
-    if (clickCount >= 3) {
-      wx.showToast({
-        title: `还需点击 ${5 - clickCount} 次进入后台`,
-        icon: 'none',
-        duration: 1000
-      });
-    }
+    wx.showToast({
+      title: `还需点击 ${5 - clickCount} 次进入后台`,
+      icon: 'none',
+      duration: 1000
+    });
 
     clickTimer = setTimeout(() => {
       clickCount = 0;
